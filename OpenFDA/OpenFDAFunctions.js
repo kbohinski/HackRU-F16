914ef3bdbdbde9f0eaf8ee8b824d7abe7ebd68a2
Kevin Bohinski <kbohinski@gmail.com>
2016-10-22 19:47:04 -0400

    exports

--- OpenFDA/OpenFDAFunctions.js
+++ OpenFDA/OpenFDAFunctions.js
@@ -3,10 +3,10 @@ var req = require('sync-request');
 var BASE_URL_LABEL = 'https://api.fda.gov/drug/label.json';
 var BASE_URL_EVENT = 'https://api.fda.gov/drug/event.json';
 
-function getDrugLabel(drug) {
+var exports.getDrugLabel = function(drug) {
   return req('GET', BASE_URL_LABEL + '?search=' + drug).getBody();
 }
 
-function getDrugEvent(drug) {
+exports.getDrugEvent = function(drug) {
   return req('GET', BASE_URL_EVENT + '?search=' + drug).getBody();
 }
